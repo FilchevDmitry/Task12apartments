ce4b1f08390451b285a166abc09655126d6808ad
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-04 15:31:25 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string room[10];
+    int num=0;
+    for(int i=0;i<10;i++){
+        std::cout<<"Input last name : ";
+        std::cin>>room[i];
+    }
+    for (int i = 0; i < 3; ++i)
+    {
+        std::cout<<"Input apartment number from 1 to 10 : ";
+        std::cin>>num;
+        while(num<0||num>10){
+            std::cout<<" Wrong!!! Input apartment number from 1 to 10 : ";
+            std::cin>>num;
+        }
+        std::cout<<room[num-1]<<"\n";
+    }
     return 0;
 }
